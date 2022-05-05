79c7487700edd2453becd31dca2e58d63dc094b9
Augusto Fernandes Costa <augusto.fcosta1808@gmail.com>
2022-05-05 19:24:41 -0300

    DDL modificando tabelas

--- SCRIPTS/scripts 2022.sql
+++ SCRIPTS/scripts 2022.sql
@@ -1693,20 +1693,91 @@ SELECT * FROM CLIENTE;
 /* FILTROS */
 
 SELECT * FROM CLIENTE
-WHERE SEXO ='M';
+WHERE SEXO = 'M';
 
 /* UPDATE */
-INSERT INTO CLIENTE VALUES (NULL, 'XX', 'M', NULL, 'XX');
-SELECT * FROM CLIENTE;
-
-DELETE FROM CLIENTE CLIENTE 
-WHERE IDCLIENTE=8;
 
 SELECT * FROM CLIENTE
 WHERE IDCLIENTE = 7;
 
-UPDATE CLIENTE 
-SET SEXO ='F'
+UPDATE CLIENTE
+SET SEXO = 'F'
 WHERE IDCLIENTE = 7;
 
-/* DELETE */
+/* DELETE */ 
+
+INSERT INTO CLIENTE VALUES(NULL,'XXX','M',NULL,'XXX');
+
+SELECT * FROM CLIENTE
+WHERE IDCLIENTE = 8;
+
+DELETE FROM CLIENTE WHERE IDCLIENTE = 8;
+
+/*
+DDL - DATA DEFINITION LANGUAGE
+*/
+
+CREATE TABLE PRODUTO (
+  IDPRODUTO INT PRIMARY KEY AUTO_INCREMENT,
+  NOME_PRODUTO VARCHAR(30) NOT NULL,
+  PRECO INT,
+  FRETE FLOAT(10,2) NOT NULL
+);
+
+/* ALTER TABLE */
+
+/* ALTERANDO O NOME DE UMA COLUNA - CHANGE */
+
+ALTER TABLE PRODUTO
+CHANGE PRECO VALOR_UNITARIO INT NOT NULL;
+
+DESC PRODUTO;
++----------------+-------------+------+-----+---------+----------------+
+| Field          | Type        | Null | Key | Default | Extra          |
++----------------+-------------+------+-----+---------+----------------+
+| IDPRODUTO      | int         | NO   | PRI | NULL    | auto_increment |
+| NOME_PRODUTO   | varchar(30) | NO   |     | NULL    |                |
+| VALOR_UNITARIO | int         | NO   |     | NULL    |                |
+| FRETE          | float(10,2) | NO   |     | NULL    |                |
++----------------+-------------+------+-----+---------+----------------+
+
+ALTER TABLE PRODUTO
+CHANGE VALOR_UNITARIO VALOR_UNITARIO INT;
+
+DESC PRODUTO;
++----------------+-------------+------+-----+---------+----------------+
+| Field          | Type        | Null | Key | Default | Extra          |
++----------------+-------------+------+-----+---------+----------------+
+| IDPRODUTO      | int         | NO   | PRI | NULL    | auto_increment |
+| NOME_PRODUTO   | varchar(30) | NO   |     | NULL    |                |
+| VALOR_UNITARIO | int         | YES  |     | NULL    |                |
+| FRETE          | float(10,2) | NO   |     | NULL    |                |
++----------------+-------------+------+-----+---------+----------------+
+
+/* MODIFY - ALTERANDO O TIPO */
+
+ALTER TABLE PRODUTO
+MODIFY VALOR_UNITARIO VARCHAR(50) NOT NULL;
+
+/* ADICIONANDO COLUNAS */
+
+ALTER TABLE PRODUTO
+ADD PESO FLOAT(10,2) NOT NULL;
+
+/* APAGANDO UMA COLUNA */
+
+ALTER TABLE PRODUTO 
+DROP COLUMN PESO;
+
+/* ADICIONANDO UMA COLUNA NA ORDEM ESPECIFICA */
+
+ALTER TABLE PRODUTO 
+ADD COLUMN PESO FLOAT(10, 2) NOT NULL
+AFTER NOME_PRODUTO;
+
+ALTER TABLE PRODUTO 
+DROP COLUMN PESO;
+
+ALTER TABLE PRODUTO 
+ADD COLUMN PESO FLOAT(10, 2) NOT NULL
+FIRST; /*COLOCANDO A COLUNA COMO PRIMEIRA*/
